7c8fde8e441412caf89062ffe69771794b184bfa
Tahsin Rahman <tr174@duke.edu>
2025-04-13 01:08:23 -0400

    odds backend

---
+++ odds_backend/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sbrscrape import Scoreboard
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/get-odds")
+def get_odds():
+    sb = Scoreboard(sport="NBA")
+    games = sb.games if hasattr(sb, 'games') else []
+
+    results = []
+    for game in games:
+        home = game["home_team"].replace("Los Angeles Clippers", "LA Clippers")
+        away = game["away_team"].replace("Los Angeles Clippers", "LA Clippers")
+        data = {
+            "home_team": home,
+            "away_team": away,
+            "moneylines": {
+                "home": game["home_ml"],
+                "away": game["away_ml"]
+            },
+            "totals": game["total"]
+        }
+        results.append(data)
+    return {"games": results}
